c5060d4f33e5f894e7f18638b313412cae702710
viktoria <vicktoriakushko@gmail.com>
2023-09-17 22:13:17 +0300

    hw 2 completed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-'use strict';
+let firstNumber = parseFloat(prompt('Введите первое число'));
+let secondNumber = parseFloat(prompt('Введите второе число'));
 
-// Task #1
-let name = 'Джон';
-let admin = name;
-alert(admin);
-
-
-// Task #2
-let planetName = 'Земля';
-let visitorName = 'Вика';
-console.log(planetName);
-console.log(visitorName);
-
-
-
-// Task #3
-let result = '10' + 10;
-console.log(result);
-console.log(typeof result);
-// В консоли ответ 1010. JavaScript автоматически конвертирует число 10 в строку и осуществляет конкатенацию двух строк
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(firstNumber * secondNumber);
+console.log(firstNumber / secondNumber);
 
 
 
