00f16dcddf50343a4aafabf9bc95739c8a9b1f9a
viktoria <vicktoriakushko@gmail.com>
2023-09-18 21:10:49 +0300

    hw 4 completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 let firstNumber = parseFloat(prompt('Введите первое число'));
 let secondNumber = parseFloat(prompt('Введите второе число'));
+let mathematicalOperation = prompt('Что нужно сделать (add, sub, mult, div)?');
+let operationSymbol;
 
-console.log(firstNumber + secondNumber);
-console.log(firstNumber - secondNumber);
-console.log(firstNumber * secondNumber);
-console.log(firstNumber / secondNumber);
+if(mathematicalOperation === 'add'){
+    result = firstNumber + secondNumber;
+    operationSymbol = "+";
+} else if(mathematicalOperation === 'sub'){
+    result = firstNumber - secondNumber;
+    operationSymbol = "-";
+} else if(mathematicalOperation === 'mult'){
+    result = firstNumber * secondNumber;
+    operationSymbol = "*";
+} else if(mathematicalOperation === 'div'){
+    result = firstNumber / secondNumber;
+    operationSymbol = "/";
+}
+alert(firstNumber + " " + operationSymbol + " " + secondNumber + " = " + result);
 
 
 
