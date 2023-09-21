928ee84fe6a2d2c84faf9d76713c7a637ed69ce3
viktoria <vicktoriakushko@gmail.com>
2023-09-21 15:27:42 +0300

    hw 3 update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-let firstNumber = parseFloat(prompt('Введите первое число '));
-let secondNumber = parseFloat(prompt('Введите второе число '));
+'use strict';
 
-console.log(firstNumber + secondNumber);
-console.log(firstNumber - secondNumber);
-console.log(firstNumber * secondNumber);
-console.log(firstNumber / secondNumber);
+const firstNumber = parseFloat(prompt('Введите первое число '));
+const secondNumber = parseFloat(prompt('Введите второе число '));
+
+const sum = firstNumber + secondNumber;
+console.log(`Результат: ${firstNumber} + ${secondNumber} = ${sum}`);
+
+const difference = firstNumber - secondNumber;
+console.log(`Результат: ${firstNumber} - ${secondNumber} = ${difference}`);
+
+const multiplication = firstNumber * secondNumber;
+console.log(`Результат: ${firstNumber} * ${secondNumber} = ${multiplication}`);
+
+
+if(secondNumber && firstNumber !== 0){
+    const division = firstNumber / secondNumber;
+    console.log(`Результат: ${firstNumber} / ${secondNumber} = ${division}`);
+}else{
+    console.log('На ноль делить нельзя!!!!!!!!!!!');
+}
+
 
 
 
