e60653b93cd299dda5c712fab754de6d5aa58245
viktoria <vicktoriakushko@gmail.com>
2023-09-30 11:36:31 +0300

    hw 5 completed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,7 @@
 'use strict';
 
-// Task#1
-let visitor = {};
-visitor.name = 'John';
-visitor.surname = 'Smith';
-visitor.name = 'Pete';
-delete visitor.name;
-console.log(visitor);
-
-// Task #2
-const user = {
-    name: "John"
-};
-// Це працюватиме?
-user.name = "Pete";
-console.log(user);
-// Да,все будет работать, консоль выведет "Pete"
-
-// Task #3
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
-let sum = salaries.Ann + salaries.John + salaries.Pete;
-console.log(sum);
-
-
-
-
+Boolean(Number('10')) + 1; // 2
+'sub' + Number(false); // sub0
+16 * '91'  //1456
+true-70  // -69
+Номер(1 + String(1)) + 1 // Будет ошибка
