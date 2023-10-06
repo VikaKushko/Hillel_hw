45ae5ff4e0955cf2ca7aad34c9dbf353b3193428
viktoria <vicktoriakushko@gmail.com>
2023-10-06 16:46:22 +0300

    hw 12 completed

--- js/script.js
+++ js/script.js
@@ -1,62 +1,98 @@
 'use strict';
 
-let birthYear = prompt('Введіть рік народження ');
-let city = prompt('В якому місті Ви живете? ');
-let sport = prompt('Який улюблений вид спорту?');
-let message;
-let sportMessage = '';
-let cityMessage = '';
-let year = 2023 - birthYear;
+// Task№1
+let a = Number(prompt('Введите число'));
+let aCheck = a === 0 ? 'Вірно' : 'Невірно';
 
+console.log(aCheck);
 
-if (birthYear === null || city === null || sport === null){
-    message = 'Шкода, що Ви не захотіли ввести  ';
-    if (birthYear === null){
-        alert(message + 'свій рік народження');
-    } else if (city === null){
-        alert(message + 'своє місто');
-    }else {
-        alert(message + 'свій вид спорту');
-    }
-} else{
-    if (city === 'Київ' || city === 'Вашингтон' || city === 'Лондон') {
-        switch (city) {
-            case 'Київ':
-                cityMessage = 'Ти живеш у столиці України';
-                break;
-            case 'Вашингтон':
-                cityMessage = 'Ти живеш у столиці США';
-                break;
-            case 'Лондон':
-                cityMessage = 'Ти живеш у столиці Великої Британії';
-                break;
-        }
-    } else {
-        cityMessage = 'Ти живеш у місті'  + city;
-    }
-    if (sport === 'Футбол' || sport === 'Баскетбол' || sport === 'Теніс') {
-        switch (sport) {
-            case 'Футбол':
-                sportMessage = 'Круто! Хочеш стати Ліонель Мессі?';
-                break;
-            case 'Баскетбол':
-                sportMessage = 'Круто! Хочеш стати Леброн Джеймс?';
-                break;
-            case 'Теніс':
-                sportMessage = 'Круто! Хочеш стати Рафаель Надаль?';
-                break;
-        }
-    } else {
-        sportMessage = 'Круто! Твій улюблений вид спорту ' + sport;
-    }
-    alert('Твій вік ' + year + '\n' + cityMessage + '\n' + sportMessage );
+// Task№2
+let a = Number(prompt('Введите число'));
+let aCheck = a > 0 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№3
+let a = Number(prompt('Введите число'));
+let aCheck = a < 0 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№4
+let a = Number(prompt('Введите число'));
+let aCheck = a >= 0 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№5
+let a = Number(prompt('Введите число'));
+let aCheck = a <= 0 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№6
+let a = Number(prompt('Введите число'));
+let aCheck = a !== 0 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№7
+let a = prompt('Введите число');
+let aCheck = a === 'test' ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№8
+let a = prompt('Введите число');
+let aCheck = a === '1' ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№9
+let a = prompt('Введите число');
+let aCheck = a > 0 && a < 5 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№10
+let a = Number(prompt('Введите число'));
+let aCheck = a === 0 || a === 2  ? a + 7  :  a / 10;
+
+console.log(aCheck);
+
+// Task№11
+let a = Number(prompt('Введите число'));
+let b = Number(prompt('Введите число'));
+let aCheck = a <= 1 && b >= 3  ? a + b  :  a - b;
+
+console.log(aCheck);
+
+// Task№12
+let a = Number(prompt('Введите число'));
+let b = Number(prompt('Введите число'));
+let aCheck = a > 2 && a < 11 || b >= 6 && b < 14 ? 'Вірно' : 'Невірно';
+
+console.log(aCheck);
+
+// Task№13
+let num = Number(prompt('Введите число 1 или 2 или 3 или 4 '));
+let result;
+
+switch (num){
+    case 1:
+        result = 'Зима';
+        break;
+    case 2:
+        result = 'Весна';
+        break;
+    case 3:
+        result = 'Лето';
+        break;
+    default:
+        result = 'Осень';
 }
 
-
-
-
-
-
+alert(result);
 
 
 
