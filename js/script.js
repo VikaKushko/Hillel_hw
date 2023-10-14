886e7977fd277839494d59a3e02d4a14b8a77996
viktoria <vicktoriakushko@gmail.com>
2023-10-14 09:34:25 +0300

    hw 13 completed

--- js/script.js
+++ js/script.js
@@ -1,101 +1,54 @@
+'use strict';
 
-// Task№1
-let a = Number(prompt('Введите число'));
-let aCheck = a === 0 ? 'Вірно' : 'Невірно';
 
-console.log(aCheck);
-
-// Task№2
-let a = Number(prompt('Введите число'));
-let aCheck = a > 0 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№3
-let a = Number(prompt('Введите число'));
-let aCheck = a < 0 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№4
-let a = Number(prompt('Введите число'));
-let aCheck = a >= 0 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№5
-let a = Number(prompt('Введите число'));
-let aCheck = a <= 0 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№6
-let a = Number(prompt('Введите число'));
-let aCheck = a !== 0 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№7
-let a = prompt('Введите число');
-let aCheck = a === 'test' ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№8
-let a = prompt('Введите число');
-let aCheck = a === '1' ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№9
-let a = prompt('Введите число');
-let aCheck = a > 0 && a < 5 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№10
-let a = Number(prompt('Введите число'));
-let aCheck = a === 0 || a === 2  ? a + 7  :  a / 10;
-
-console.log(aCheck);
-
-// Task№11
-let a = Number(prompt('Введите число'));
-let b = Number(prompt('Введите число'));
-let aCheck = a <= 1 && b >= 3  ? a + b  :  a - b;
-
-console.log(aCheck);
-
-// Task№12
-let a = Number(prompt('Введите число'));
-let b = Number(prompt('Введите число'));
-let aCheck = a > 2 && a < 11 || b >= 6 && b < 14 ? 'Вірно' : 'Невірно';
-
-console.log(aCheck);
-
-// Task№13
-let num = Number(prompt('Введите число 1 или 2 или 3 или 4 '));
-let result;
-
-switch (num){
-    case 1:
-        result = 'Зима';
-        break;
-    case 2:
-        result = 'Весна';
-        break;
-    case 3:
-        result = 'Лето';
-        break;
-    default:
-        result = 'Осень';
+// Task 1
+for (let i = 20; i <= 30; i = i + 0.5){
+    console.log(i);
 }
 
-alert(result);
+// Task 2
+for (let i = 10; i <= 100; i += 10){
+    let s = i * 27;
+    console.log(s);
+}
 
+// Task 3
+for (let i = 10; i <= 100; i += 10) {
+    let s = i * 27;
+    console.log(s);
+}
 
+// Task 4
+let n = 1;
 
+if (n <= 1){
+    console.log('Число не є простим');
+}else {
+    let k = true;
+    for (let i = 2; i <= n; i++) {
+        if (n % i === 0){
+            k = false;
+        }
+    }
+    if (k) {
+        console.log('Число є простим');
+    } else {
+        console.log('Число не є простим');
+    }
+}
 
+// Task 5
+let n = 10;
+let b = 1;
 
+while (b < n) {
+    b *= 3;
+}
+
+if (b === n) {
+    console.log("Можна отримати це число зведенням числа 3 у деякий ступінь");
+} else {
+    console.log("Не можна отримати це число зведенням числа 3 у деякий ступінь");
+}
 
 
