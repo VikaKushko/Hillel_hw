66e78ff2960a4d1966bb56e4a5d9a19531238423
viktoria <vicktoriakushko@gmail.com>
2023-10-29 20:29:42 +0200

    hw 27 completed

--- js/script.js
+++ js/script.js
@@ -1,47 +1,72 @@
-// Task 1
-const exponentiation = function (n, d) {
-  return n ** d;
-};
+const users = [
+  {
+    index: 0,
+    isActive: true,
+    balance: "$2,226.60",
+    name: "Eugenia Sawyer",
+    gender: "female",
+    phone: "+1 (840) 583-3207",
+    address: "949 John Street, Rose, Puerto Rico, 1857",
+  },
+  {
+    index: 1,
+    isActive: true,
+    balance: "$2,613.77",
+    name: "Pauline Gallegos",
+    gender: "female",
+    phone: "+1 (985) 593-3328",
+    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
+  },
+  {
+    index: 2,
+    isActive: false,
+    balance: "$3,976.41",
+    name: "Middleton Chaney",
+    gender: "male",
+    phone: "+1 (995) 591-2478",
+    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
+  },
+  {
+    index: 3,
+    isActive: true,
+    balance: "$1,934.58",
+    name: "Burns Poole",
+    gender: "male",
+    phone: "+1 (885) 559-3422",
+    address: "730 Seba Avenue, Osage, Alabama, 6290",
+  },
+  {
+    index: 4,
+    isActive: true,
+    balance: "$3,261.65",
+    name: "Mcfadden Horne",
+    gender: "male",
+    phone: "+1 (942) 565-3988",
+    address: "120 Scholes Street, Kirk, Michigan, 1018",
+  },
+  {
+    index: 5,
+    isActive: false,
+    balance: "$1,790.56",
+    name: "Suzette Lewis",
+    gender: "female",
+    phone: "+1 (837) 586-3283",
+    address: "314 Dunne Place, Bawcomville, Guam, 9053",
+  },
+];
 
-function mainFunction(callback) {
-  const num = prompt("Enter number");
-  const degree = prompt("Enter degree");
-  return callback(num, degree);
-}
-alert(mainFunction(exponentiation));
+const phoneNumbers = [];
+let totalBalance = 0;
 
-// Task 2
-const multiplay = function (n, m) {
-  return n * m;
-};
+users.forEach((user) => {
+  const balance = parseFloat(user.balance.replace("$", "").replace(",", ""));
 
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(multiplay));
+  if (balance > 2000) {
+    phoneNumbers.push(user.phone);
+  }
 
-// Task 3
-const division = function (n, d) {
-  return n / d;
-};
+  totalBalance += balance;
+});
 
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(division));
-
-// Task 4
-const modulo = function (n, d) {
-  return n % d;
-};
-
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(modulo));
+console.log(phoneNumbers);
+console.log(totalBalance);
