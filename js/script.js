2ce5bd2c6bedd04053a82fdee1e04375868e7f2a
viktoria <vicktoriakushko@gmail.com>
2023-10-30 21:38:42 +0200

    hw 25 update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,8 @@
-// Task 1
-const exponentiation = function (n, d) {
-  return n ** d;
-};
+const num = prompt("Enter");
+const arrayOfDigits = num.split(" ").map(Number && String);
 
-function mainFunction(callback) {
-  const num = prompt("Enter number");
-  const degree = prompt("Enter degree");
-  return callback(num, degree);
-}
-alert(mainFunction(exponentiation));
+arrayOfDigits.sort();
+console.log(arrayOfDigits);
 
-// Task 2
-const multiplay = function (n, m) {
-  return n * m;
-};
-
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(multiplay));
-
-// Task 3
-const division = function (n, d) {
-  return n / d;
-};
-
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(division));
-
-// Task 4
-const modulo = function (n, d) {
-  return n % d;
-};
-
-function mainFunction(callback) {
-  const numOne = prompt("Enter number");
-  const numTwo = prompt("Enter number");
-  return callback(numOne, numTwo);
-}
-alert(mainFunction(modulo));
+arrayOfDigits.splice(1, 4);
+console.log(arrayOfDigits);
