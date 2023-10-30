39d7957ef611341306dbd00ec1a45df0035a9745
viktoria <vicktoriakushko@gmail.com>
2023-10-30 15:35:09 +0200

    hw 26 completed

--- js/script.js
+++ js/script.js
@@ -1,72 +1,52 @@
-const users = [
-  {
-    index: 0,
-    isActive: true,
-    balance: "$2,226.60",
-    name: "Eugenia Sawyer",
-    gender: "female",
-    phone: "+1 (840) 583-3207",
-    address: "949 John Street, Rose, Puerto Rico, 1857",
-  },
-  {
-    index: 1,
-    isActive: true,
-    balance: "$2,613.77",
-    name: "Pauline Gallegos",
-    gender: "female",
-    phone: "+1 (985) 593-3328",
-    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
-  },
-  {
-    index: 2,
-    isActive: false,
-    balance: "$3,976.41",
-    name: "Middleton Chaney",
-    gender: "male",
-    phone: "+1 (995) 591-2478",
-    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
-  },
-  {
-    index: 3,
-    isActive: true,
-    balance: "$1,934.58",
-    name: "Burns Poole",
-    gender: "male",
-    phone: "+1 (885) 559-3422",
-    address: "730 Seba Avenue, Osage, Alabama, 6290",
-  },
-  {
-    index: 4,
-    isActive: true,
-    balance: "$3,261.65",
-    name: "Mcfadden Horne",
-    gender: "male",
-    phone: "+1 (942) 565-3988",
-    address: "120 Scholes Street, Kirk, Michigan, 1018",
-  },
-  {
-    index: 5,
-    isActive: false,
-    balance: "$1,790.56",
-    name: "Suzette Lewis",
-    gender: "female",
-    phone: "+1 (837) 586-3283",
-    address: "314 Dunne Place, Bawcomville, Guam, 9053",
-  },
+const arr = [
+  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
+  76, -4, 12, -35, 4, 47,
 ];
 
-const phoneNumbers = [];
-let totalBalance = 0;
+// Знайти суму та кількість позитивних елементів
+const positiveNumbers = arr.filter((num) => num > 0);
+const sumPositives = positiveNumbers.reduce((sum, num) => sum + num, 0);
+const countPositives = positiveNumbers.length;
 
-users.forEach((user) => {
-  const balance = parseFloat(user.balance.replace("$", "").replace(",", ""));
+// Знайти мінімальний елемент масиву та його порядковий номер.
+const minElement = Math.min(...arr);
+const minElementIndex = arr.indexOf(minElement);
 
-  if (balance > 2000) {
-    phoneNumbers.push(user.phone);
-  }
+// Знайти максимальний елемент масиву та його порядковий номер.
+const maxElement = Math.max(...arr);
+const maxElementIndex = arr.indexOf(maxElement);
 
-  totalBalance += balance;
-});
+// Визначити кількість негативних елементів.
+const countNegatives = arr.filter((num) => num < 0).length;
 
-console.log(phoneNumbers);
-console.log(totalBalance);
+// Знайти кількість парних позитивних елементів.
+const countPositives = arr.filter((num) => num > 0).length;
+
+// Знайти суму парних позитивних елементів.
+const evenElements = arr.filter((num) => num > 0 && num % 2 === 0);
+const sumEvenPositives = evenElements.reduce((sum, num) => sum + num, 0);
+const countEvenPositives = evenElements.length;
+
+// Знайти суму непарних позитивних елементів.
+const evenElements = arr.filter((num) => num < 0 && num % 2 !== 0);
+const sumEvenNegatives = evenElements.reduce((sum, num) => sum + num, 0);
+
+// Знайти добуток позитивних елементів.
+const evenElements = arr.filter((num) => num > 0);
+const mulEvenPositives = evenElements.reduce((sum, num) => sum * num, 1);
+
+// Знайти найбільший серед елементів масиву, ост альні обнулити.
+const maxElementZeroed = Math.max(...arr);
+const arra = arr.map((num) => (num === maxElementZeroed ? num : 0));
+
+console.log(sumPositives);
+console.log(countPositives);
+console.log(minElementIndex);
+console.log(maxElementIndex);
+console.log(countNegatives);
+console.log(countPositives);
+console.log(sumEvenPositives);
+console.log(countEvenPositives);
+console.log(sumEvenNegatives);
+console.log(mulEvenPositives);
+console.log(arra);
