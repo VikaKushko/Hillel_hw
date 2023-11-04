608f3fdb6b327fb76b93766afd290e034bc8182b
viktoria <vicktoriakushko@gmail.com>
2023-11-04 10:36:05 +0200

    hw 29 completed

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const num = prompt("Enter");
-const arrayOfDigits = num.split(" ").map(Number && String);
-
-arrayOfDigits.sort();
-console.log(arrayOfDigits);
-
-arrayOfDigits.splice(1, 4);
-console.log(arrayOfDigits);
