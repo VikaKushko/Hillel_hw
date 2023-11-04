ba4edfba1b78746981d012b3ee0bf094e0f57d96
viktoria <vicktoriakushko@gmail.com>
2023-11-04 12:30:59 +0200

    hw 30 completed

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
-// Task #1
-const getFactorial = (num) => {
-  if (num !== 1) {
-    return num * getFactorial(num - 1);
-  } else {
-    return num;
+const arr = [1, 2, 3, 4, {}, [1, 2, 3], [1, 2, 3], [3, 4, [5, 6, 7]]];
+
+function flat(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      res = res.concat(flat(arr[i]));
+    } else {
+      res.push(arr[i]);
+    }
   }
-};
+  return res;
+}
 
-console.log(getFactorial(3));
-
-// Task #2
-const pow = (num, degree) => {
-  if (degree === 0) {
-    return 1;
-  }
-  return num * pow(num, degree - 1);
-};
-
-console.log(pow(2, 3));
-
-// Task #3
-const inte = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return inte(a + 1, b - 1);
-};
-
-console.log(inte(5, 3));
+console.log(flat(arr));
+console.log(flat(arr));
