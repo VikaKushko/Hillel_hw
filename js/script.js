dd3d4954015f78aa167ed79126c82e89ed07230d
viktoria <vicktoriakushko@gmail.com>
2023-11-21 06:44:33 +0200

    hw 36 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-const list = document.querySelectorAll("li");
-
-for (const item of list) {
-  console.log(item.textContent);
-}
-
-console.log(`Количество елементов ${list.length}`);
-
+const list = document.querySelector("ul");
 const arr = [];
-for (const item of list) {
-  arr.push(item.textContent);
+
+for (const attribute of list.attributes) {
+  arr.push(attribute.value);
 }
+
 console.log(arr);
+
+const list = document.getElementById("ulId");
+const lastElement = list.lastElementChild;
+lastElement.textContent = "Привет меня зовут Vika";
+
+const list = document.getElementById("ulId");
+const firstElement = list.firstElementChild;
+firstElement.setAttribute("data-my-name", "vika");
+
+const list = document.getElementById("ulId");
+list.removeAttribute("data-dog-tail");
