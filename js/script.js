fa56764b3edcb9b11edbb1689f9d6c26cd1b21af
viktoria <vicktoriakushko@gmail.com>
2023-11-24 18:48:14 +0200

    hw 40 completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const name = "img/";
   const arrImg = ["1.jpeg", "2.jpeg", "3.jpeg, '4.jpeg", "5.jpeg"];
 
-  const random = Math.random() * arrImg.length;
+  const random = Math.floor(Math.random() * arrImg.length);
 
   const result = name + arrImg[random];
 
