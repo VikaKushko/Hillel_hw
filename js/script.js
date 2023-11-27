c781290934bea3fe7bf928e7b6f3447267644cca
viktoria <vicktoriakushko@gmail.com>
2023-11-27 22:28:41 +0200

    hw 41 completed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,47 @@
+// // Task1
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.querySelector("[data-modal]");
+  const modalImg = document.querySelector("[data-modal-img]");
+  const modalClose = document.querySelector("[data-modal-close]");
+  const galleryImg = document.querySelector("[data-img]");
+
+  function openModal() {
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    modal.style.display = "none";
+  }
+
+  // Додайте подію кліку на зображення галереї
+  galleryImg.addEventListener("click", function () {
+    modalImg.src = this.src;
+    openModal();
+  });
+
+  modalClose.addEventListener("click", closeModal);
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+});
+
+//Task2
 document.addEventListener("DOMContentLoaded", () => {
-  const name = "img/";
-  const arrImg = ["1.jpeg", "2.jpeg", "3.jpeg, '4.jpeg", "5.jpeg"];
+  const t = document.querySelector(".tabs");
 
-  const random = Math.floor(Math.random() * arrImg.length);
+  t.addEventListener("click", (event) => {
+    const clickTb = event.target.closest(".tab");
 
-  const result = name + arrImg[random];
+    const getId = clickTb.getAttribute("data-tab-id");
+    const r = document.querySelectorAll(".content");
 
-  const image = document.querySelector("[data-ren-img]");
+    r.forEach((content) => {
+      content.style.display = "none";
+    });
 
-  image.src = result;
+    document.getElementById(getId).style.display = "block";
+  });
 });
