6e4f0725e2e2d5b430a594e076bc7240436c86b3
viktoria <vicktoriakushko@gmail.com>
2023-12-17 21:58:08 +0200

    hw 50 completed

--- js/script.js
+++ js/script.js
@@ -1,95 +1,166 @@
-(function () {
-  const saveColor = () => {
-    const selectedColor = document.querySelector(
-      'input[name="colorScheme"]:checked',
-    ).value;
+"use strict";
 
-    localStorage.setItem("colorScheme", selectedColor);
-    addColor();
+(function () {
+  const KEY = {
+    todoFormSelector: "#todoForm",
+    todoContainerSelector: "#todoItems",
+    dataKey: "formData",
   };
 
-  const addColor = () => {
-    const getColor = localStorage.getItem("colorScheme");
-    const darkRadio = document.querySelector('input[value="dark"]');
+  const view = {
+    renderElement(data) {
+      const template = this.createTemplate(data);
+      this.renderTodoItem(template);
+    },
 
-    if (getColor === "dark") {
-      document.body.style.backgroundColor = "rgba(43,45,48,0.96)";
-      if (darkRadio) {
-        darkRadio.checked = true;
+    renderTodoItem(elementToRender) {
+      const todoContainer = document.querySelector(KEY.todoContainerSelector);
+      todoContainer.prepend(elementToRender);
+    },
+
+    createTemplate(data) {
+      const wrapper = document.createElement("div");
+      wrapper.className = "col-4";
+      wrapper.setAttribute("data-todo-item", data.id);
+
+      const taskWrapper = document.createElement("div");
+      taskWrapper.className = "taskWrapper";
+      wrapper.appendChild(taskWrapper);
+
+      const taskHeading = document.createElement("div");
+      taskHeading.className = "taskHeading";
+      taskHeading.innerHTML = data.title;
+      taskWrapper.appendChild(taskHeading);
+
+      const taskDescription = document.createElement("div");
+      taskDescription.className = "taskDescription";
+      taskDescription.innerHTML = data.description;
+      taskWrapper.appendChild(taskDescription);
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "btn btn-sm btn-danger";
+      deleteBtn.innerText = "X";
+      deleteBtn.setAttribute("data-remove-btn", "");
+      taskWrapper.appendChild(deleteBtn);
+
+      return wrapper;
+    },
+
+    resetData() {
+      document.querySelector(KEY.todoFormSelector).reset();
+    },
+
+    removeElement(todoId) {
+      document.querySelector(`[data-todo-item="${todoId}"]`).remove();
+    },
+  };
+
+  const modal = {
+    currentId: 0,
+
+    save(data) {
+      ++this.currentId;
+      const dataCopy = { ...data };
+      dataCopy.id = this.currentId;
+      const savedData = this.get();
+      savedData.push(dataCopy);
+      try {
+        localStorage.setItem(KEY.dataKey, JSON.stringify(savedData));
+        return savedData[savedData.length - 1];
+      } catch (e) {
+        return false;
       }
-    } else {
-      document.body.style.backgroundColor = "#ffffff";
-    }
-  };
+    },
 
-  const loadedHandler = () => {
-    const form = document.querySelector("#colorForm");
+    get() {
+      const savedData = JSON.parse(localStorage.getItem(KEY.dataKey));
+      return savedData ? savedData : [];
+    },
 
-    form.addEventListener("change", saveColor);
-
-    addColor();
-  };
-
-  document.addEventListener("DOMContentLoaded", loadedHandler);
-})();
-
-// Task2
-(function () {
-  const createList = () => {
-    const list = document.querySelector("#itemList");
-
-    const data = JSON.parse(localStorage.getItem("data")) || [];
-
-    list.innerHTML = "";
-
-    data.forEach((item) => {
-      const createLi = document.createElement("li");
-      createLi.textContent = item.text;
-
-      const createBtn = document.createElement("button");
-      createBtn.classList.add("favorite-btn");
-      createBtn.textContent = "Удалить из избранного";
-      createBtn.addEventListener("click", function () {
-        toggleFavorite(item.id);
+    removeElementById(todoId) {
+      const savedElements = this.get();
+      const index = savedElements.findIndex(({ id }) => {
+        return todoId === id;
       });
+      const [removedElements] = savedElements.splice(index, 1);
 
-      createLi.appendChild(createBtn);
-      list.appendChild(createLi);
-    });
+      try {
+        localStorage.setItem(KEY.dataKey, JSON.stringify(savedElements));
+        return removedElements;
+      } catch (e) {
+        return false;
+      }
+    },
+
+    initId() {
+      const items = this.get();
+      if (!items.length) return;
+      this.currentId = +items[items.length - 1].id;
+    },
   };
 
-  const toggleFavorite = (itemId) => {
-    const data = JSON.parse(localStorage.getItem("data")) || [];
+  const controller = {
+    formHandler(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      const { target } = e;
+      const data = Array.from(
+        target.querySelectorAll("input, textarea"),
+      ).reduce((acc, item) => {
+        acc[item.name] = item.value;
+        return acc;
+      }, {});
 
-    const index = data.findIndex((item) => item.id === itemId);
-    if (index !== -1) {
-      data.splice(index, 1);
-    }
+      const savedData = modal.save(data);
+      if (savedData) {
+        view.renderElement(savedData);
+        view.resetData();
+      }
+    },
 
-    localStorage.setItem("data", JSON.stringify(data));
+    removeTodoItemHandler(e) {
+      e.stopPropagation();
+      const { target } = e;
+      if (!target.hasAttribute("data-remove-btn")) return;
+      const todoId = +target
+        .closest("[data-todo-item]")
+        .getAttribute("data-todo-item");
+      const removedElement = !!modal.removeElementById(todoId);
 
-    // Обновление интерфейса
-    createList();
+      if (removedElement) {
+        view.removeElement(todoId);
+      }
+    },
+
+    loadedHandler() {
+      modal.initId();
+      const form = document.querySelector(KEY.todoFormSelector);
+      form.addEventListener("submit", controller.formHandler);
+
+      const todoContainer = document.querySelector(KEY.todoContainerSelector);
+      todoContainer.addEventListener(
+        "click",
+        controller.removeTodoItemHandler.bind(controller),
+      );
+    },
+
+    renderExistingItems() {
+      modal.get().forEach((item) => {
+        view.renderElement(item);
+      });
+    },
+
+    init() {
+      this.formHandler = this.formHandler.bind(this);
+      this.removeTodoItemHandler = this.removeTodoItemHandler.bind(this);
+      this.loadedHandler = this.loadedHandler.bind(this);
+
+      document.addEventListener("DOMContentLoaded", () => {
+        controller.loadedHandler();
+        controller.renderExistingItems();
+      });
+    },
   };
 
-  const addBtn = document.getElementById("addBtn");
-  addBtn.addEventListener("click", function () {
-    const newItemText = document.getElementById("newItemText").value;
-
-    if (newItemText) {
-      const data = JSON.parse(localStorage.getItem("data")) || [];
-      const newItem = {
-        id: Date.now(),
-        text: newItemText,
-      };
-      data.push(newItem);
-      localStorage.setItem("data", JSON.stringify(data));
-
-      createList();
-
-      document.getElementById("newItemText").value = "";
-    }
-  });
-
-  document.addEventListener("DOMContentLoaded", createList);
+  controller.init();
 })();
