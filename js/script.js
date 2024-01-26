e863e29aa02ca8c93c3ce93fd73681c037182acc
viktoria <vicktoriakushko@gmail.com>
2024-01-26 19:04:29 +0200

    hw 34 completed

--- js/script.js
+++ js/script.js
@@ -1,95 +1,31 @@
-(function () {
-  const saveColor = () => {
-    const selectedColor = document.querySelector(
-      'input[name="colorScheme"]:checked',
-    ).value;
+"use strict";
 
-    localStorage.setItem("colorScheme", selectedColor);
-    addColor();
-  };
+function insertWordInto(word) {
+  const words = word.split(" ");
 
-  const addColor = () => {
-    const getColor = localStorage.getItem("colorScheme");
-    const darkRadio = document.querySelector('input[value="dark"]');
-
-    if (getColor === "dark") {
-      document.body.style.backgroundColor = "rgba(43,45,48,0.96)";
-      if (darkRadio) {
-        darkRadio.checked = true;
-      }
-    } else {
-      document.body.style.backgroundColor = "#ffffff";
-    }
-  };
-
-  const loadedHandler = () => {
-    const form = document.querySelector("#colorForm");
-
-    form.addEventListener("change", saveColor);
-
-    addColor();
-  };
-
-  document.addEventListener("DOMContentLoaded", loadedHandler);
-})();
-
-// Task2
-(function () {
-  const createList = () => {
-    const list = document.querySelector("#itemList");
-
-    const data = JSON.parse(localStorage.getItem("data")) || [];
-
-    list.innerHTML = "";
-
-    data.forEach((item) => {
-      const createLi = document.createElement("li");
-      createLi.textContent = item.text;
-
-      const createBtn = document.createElement("button");
-      createBtn.classList.add("favorite-btn");
-      createBtn.textContent = "Удалить из избранного";
-      createBtn.addEventListener("click", function () {
-        toggleFavorite(item.id);
-      });
-
-      createLi.appendChild(createBtn);
-      list.appendChild(createLi);
-    });
-  };
-
-  const toggleFavorite = (itemId) => {
-    const data = JSON.parse(localStorage.getItem("data")) || [];
-
-    const index = data.findIndex((item) => item.id === itemId);
-    if (index !== -1) {
-      data.splice(index, 1);
+  function insert(wordTwo) {
+    if (!wordTwo || typeof wordTwo !== "string") {
+      throw new Error("Параметр повинен бути непорожнім рядком");
     }
 
-    localStorage.setItem("data", JSON.stringify(data));
+    const randomIndex = Math.floor(Math.random() * (words.length + 1));
+    words.splice(randomIndex, 0, wordTwo);
 
-    // Обновление интерфейса
-    createList();
-  };
+    const resultString = words.join(" ");
 
-  const addBtn = document.getElementById("addBtn");
-  addBtn.addEventListener("click", function () {
-    const newItemText = document.getElementById("newItemText").value;
+    return resultString;
+  }
 
-    if (newItemText) {
-      const data = JSON.parse(localStorage.getItem("data")) || [];
-      const newItem = {
-        id: Date.now(),
-        text: newItemText,
-      };
-      data.push(newItem);
-      localStorage.setItem("data", JSON.stringify(data));
+  return insert;
+}
 
-      createList();
+const insert = insertWordInto("hello world");
 
-      document.getElementById("newItemText").value = "";
-    }
-  });
+const result = insert("Odesa");
+console.log(result);
 
-  document.addEventListener("DOMContentLoaded", createList);
-})();
+const secondResult = insert("Odesa");
+console.log(secondResult);
+
+const thirdResult = insert("Odesa");
+console.log(thirdResult);
