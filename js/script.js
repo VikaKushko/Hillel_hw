813950bb2dbecdad4ece57b64d20628a300c3480
viktoria <vicktoriakushko@gmail.com>
2024-02-06 22:27:44 +0200

    hw 66 completed

--- js/script.js
+++ js/script.js
@@ -1,60 +1,15 @@
-class TaskManager {
-  constructor() {
-    this.tasks = new Map();
-  }
+"use strict";
 
-  addTask(id, description) {
-    if (this.tasks.has(id)) {
-      console.log("A task with this ID already exists");
-    }
+function analyzeText(line) {
+  const word = line.toLowerCase().split(" ");
+  const uniqueWords = new Set(word);
+  const uniqueWordsCount = uniqueWords.size;
 
-    this.tasks.set(id, description);
-  }
-
-  removeTask(id) {
-    if (!this.tasks.has(id)) {
-      console.log("The task with this ID does not exist");
-    }
-
-    this.tasks.delete(id);
-  }
-
-  findTask(id) {
-    if (this.tasks.has(id)) {
-      return this.tasks.get(id);
-    } else {
-      return "No task found";
-    }
-  }
-
-  displayTasks() {
-    for (let [id, description] of this.tasks) {
-      console.log(`ID: ${id}, Опис: ${description}`);
-    }
-  }
-
-  updateTaskDescription(id, newDescription) {
-    if (!this.tasks.has(id)) {
-      console.log("The task with this ID does not exist");
-    }
-
-    this.tasks.set(id, newDescription);
-  }
+  return uniqueWordsCount;
 }
 
-const taskManager = new TaskManager();
-
-taskManager.addTask(1, "Завдання 1");
-taskManager.addTask(2, "Завдання 2");
-taskManager.addTask(3, "Завдання 3");
-taskManager.addTask(4, "Завдання 4");
-
-taskManager.displayTasks();
-
-console.log(taskManager.findTask(2));
-
-taskManager.updateTaskDescription(2, "Оновлене завдання 2");
-
-taskManager.removeTask(3);
-console.log(taskManager.findTask(3));
-console.log(taskManager.findTask(2));
+const text = "Привет привет меня зовут Вика меня";
+const analysisResult = analyzeText(text);
+console.log(text);
+console.log("Number of unique words:", analysisResult);
+console.log("Unique words:", analysisResult);
