87e8faed27321dd9237389ebd275df1ee8c123ff
viktoria <vicktoriakushko@gmail.com>
2024-02-06 22:02:25 +0200

    hw 65 completed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,60 @@
-"use strict";
-function bind(fn, context) {
-  return function () {
-    const args = Array.from(arguments);
-    context.f = fn;
-    const result = context.f(...args);
-    delete context.f;
-    return result;
-  };
+class TaskManager {
+  constructor() {
+    this.tasks = new Map();
+  }
+
+  addTask(id, description) {
+    if (this.tasks.has(id)) {
+      console.log("A task with this ID already exists");
+    }
+
+    this.tasks.set(id, description);
+  }
+
+  removeTask(id) {
+    if (!this.tasks.has(id)) {
+      console.log("The task with this ID does not exist");
+    }
+
+    this.tasks.delete(id);
+  }
+
+  findTask(id) {
+    if (this.tasks.has(id)) {
+      return this.tasks.get(id);
+    } else {
+      return "No task found";
+    }
+  }
+
+  displayTasks() {
+    for (let [id, description] of this.tasks) {
+      console.log(`ID: ${id}, Опис: ${description}`);
+    }
+  }
+
+  updateTaskDescription(id, newDescription) {
+    if (!this.tasks.has(id)) {
+      console.log("The task with this ID does not exist");
+    }
+
+    this.tasks.set(id, newDescription);
+  }
 }
 
-const someObj = {
-  firstName: "Ihor",
-  lastName: "Cat",
-};
+const taskManager = new TaskManager();
 
-const greet = function (message) {
-  console.log(`${message}, ${this.firstName} ${this.lastName}`);
-};
+taskManager.addTask(1, "Завдання 1");
+taskManager.addTask(2, "Завдання 2");
+taskManager.addTask(3, "Завдання 3");
+taskManager.addTask(4, "Завдання 4");
 
-const boundGreet = bind(greet, someObj);
-boundGreet("Hello");
+taskManager.displayTasks();
+
+console.log(taskManager.findTask(2));
+
+taskManager.updateTaskDescription(2, "Оновлене завдання 2");
+
+taskManager.removeTask(3);
+console.log(taskManager.findTask(3));
+console.log(taskManager.findTask(2));
