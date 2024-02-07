2c43b5d07913e1df123c748a8d50aca68464470e
viktoria <vicktoriakushko@gmail.com>
2024-02-07 16:35:17 +0200

    hw 68 completed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
 "use strict";
-
-function cacheComplexCalculations(calc) {
-  const wmap = new WeakMap();
-
-  return function (obj) {
-    if (wmap.has(obj)) {
-      return wmap.get(obj);
-    }
-
-    const result = calc(obj);
-    wmap.set(obj, result);
-    return result;
-  };
+function trackObjects(obj, wSet) {
+  if (wSet.has(obj)) {
+    return true;
+  }
+  wSet.add(obj);
+  return false;
 }
 
-function calc(obj) {
-  return obj.a + obj.b;
-}
-const obj1 = { a: 2, b: 3 };
-const obj2 = { a: 7, b: 3 };
+const wSet = new WeakSet();
 
-const r = cacheComplexCalculations(calc);
-console.log(r(obj1));
-console.log(r(obj1));
-console.log(r(obj2));
-console.log(r(obj2));
+const obj1 = { id: 1 };
+const obj2 = { id: 2 };
+
+console.log(trackObjects(obj1, wSet));
+console.log(trackObjects(obj1, wSet));
+
+console.log(trackObjects(obj2, wSet));
+console.log(trackObjects(obj2, wSet));
