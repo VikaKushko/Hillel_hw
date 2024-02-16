21a617bd58a5e469cb9f20956ae57964e96c86ee
viktoria <vicktoriakushko@gmail.com>
2024-02-16 14:55:10 +0200

    hw 69 completed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,72 @@
 "use strict";
-function trackObjects(obj, wSet) {
-  if (wSet.has(obj)) {
-    return true;
-  }
-  wSet.add(obj);
-  return false;
+
+function fetchPost(postId) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Post not found");
+        }
+        return response.json();
+      })
+      .then((post) => resolve(post))
+      .catch((err) => reject(err));
+  });
 }
 
-const wSet = new WeakSet();
+function fetchComments(postId) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Comments not found");
+        }
+        return response.json();
+      })
+      .then((comments) => resolve(comments))
+      .catch((err) => reject(err));
+  });
+}
 
-const obj1 = { id: 1 };
-const obj2 = { id: 2 };
+function displayPost(post) {
+  const postContainer = document.getElementById("postContainer");
+  postContainer.innerHTML = `
+    <div>
+      <h2>${post.title}</h2>
+      <p>${post.body}</p>
+      <button id="commentsButton">Get Comments</button>
+    </div>
+  `;
 
-console.log(trackObjects(obj1, wSet));
-console.log(trackObjects(obj1, wSet));
+  const commentsButton = document.getElementById("commentsButton");
+  commentsButton.addEventListener("click", function () {
+    fetchComments(post.id)
+      .then((comments) => displayComments(comments))
+      .catch((error) => alert(error.message));
+  });
+}
 
-console.log(trackObjects(obj2, wSet));
-console.log(trackObjects(obj2, wSet));
+function displayComments(comments) {
+  const postContainer = document.getElementById("postContainer");
+  const commentsList = document.createElement("ul");
+  comments.forEach((comment) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${comment.name}: ${comment.body}`;
+    commentsList.appendChild(listItem);
+  });
+  postContainer.appendChild(commentsList);
+}
+
+function searchPost() {
+  const postId = document.getElementById("postIdInput").value;
+  if (!postId) {
+    alert("Please enter a post ID.");
+    return;
+  }
+
+  fetchPost(postId)
+    .then((post) => {
+      displayPost(post);
+    })
+    .catch((error) => alert(error.message));
+}
